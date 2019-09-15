87552ec71a8e28a061fd541da613a71e27a28f83
KPennart <kpennart@uncc.edu>
2019-09-15 02:04:02 -0400

    Added Movement and Camera Functions
    Added Script to handle mouse movement, currently the camera is in an awkward testing position. I will work on getting the camera in a good first person view.
    
    Added script to handle character movement.
    
    Added Animations to Vince and fixed his model
    
    Added testing plane to walk around on.
    
    Added crouching.
    
    Controls:
    WASD (movement)
    Left Shift (sprint)
    Left Ctrl (crouch)

---
+++ Itcs 4231 Game Project/Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [SerializeField] private string mouseX_str, mouseY_str;
+    [SerializeField] private float mouseSensitivity_flt;
+
+    [SerializeField] private Transform playerBody;
+
+    private float xAxisClamp_flt;
+
+    private void Awake()
+    {
+        LockCursor();
+        xAxisClamp_flt = 0.0f;
+    }
+
+
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        float mouseX_flt = Input.GetAxis(mouseX_str) * mouseSensitivity_flt * Time.deltaTime;
+        float mouseY_flt = Input.GetAxis(mouseY_str) * mouseSensitivity_flt * Time.deltaTime;
+
+        xAxisClamp_flt += mouseY_flt;
+
+        if(xAxisClamp_flt > 90.0f)
+        {
+            xAxisClamp_flt = 90.0f;
+            mouseY_flt = 0.0f;
+            ClampXAxisRotationToValue(270.0f);
+        }
+        else if (xAxisClamp_flt < -90.0f)
+        {
+            xAxisClamp_flt = -90.0f;
+            mouseY_flt = 0.0f;
+            ClampXAxisRotationToValue(90.0f);
+        }
+
+        transform.Rotate(Vector3.left * mouseY_flt);
+        playerBody.Rotate(Vector3.up * mouseX_flt);
+    }
+
+    private void ClampXAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+}
---
+++ Itcs 4231 Game Project/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool isCrouching_bool;
+
+    private float speed;
+    private float walkSpeed = 0.05f;
+    private float runSpeed = 0.1f;
+    private float crouchSpeed = 0.025f;
+    private float rotationSpeed;
+
+    Rigidbody rb;
+    Animator anim;
+    CapsuleCollider col_size;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        col_size = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            ToggleCrouch();
+        }
+
+        var z = Input.GetAxis("Vertical") * speed;
+        var y = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        transform.Translate(0, 0, z);
+        transform.Translate(0, y, 0);
+
+        if (isCrouching_bool)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                anim.SetBool("isWalking", true);
+                anim.SetBool("isRunning", false);
+                anim.SetBool("isIdle", false);
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                anim.SetBool("isWalking", true);
+                anim.SetBool("isRunning", false);
+                anim.SetBool("isIdle", false);
+            }
+            else
+            {
+                anim.SetBool("isWalking", false);
+                anim.SetBool("isRunning", false);
+                anim.SetBool("isIdle", true);
+            }
+        }
+        else if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                anim.SetBool("isWalking", false);
+                anim.SetBool("isRunning", true);
+                anim.SetBool("isIdle", false);
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                anim.SetBool("isWalking", false);
+                anim.SetBool("isRunning", true);
+                anim.SetBool("isIdle", false);
+            }
+            else
+            {
+                anim.SetBool("isWalking", false);
+                anim.SetBool("isRunning", false);
+                anim.SetBool("isIdle", true);
+            }
+        }
+        else if (!isCrouching_bool)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                anim.SetBool("isWalking", true);
+                anim.SetBool("isRunning", false);
+                anim.SetBool("isIdle", false);
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                anim.SetBool("isWalking", true);
+                anim.SetBool("isRunning", false);
+                anim.SetBool("isIdle", false);
+            }
+            else
+            {
+                anim.SetBool("isWalking", false);
+                anim.SetBool("isRunning", false);
+                anim.SetBool("isIdle", true);
+            }
+        }
+    }
+
+    private void ToggleCrouch()
+    {
+        if (isCrouching_bool)
+        {
+            isCrouching_bool = false;
+            anim.SetBool("isCrouching", false);
+            col_size.height = 2;
+            col_size.center = new Vector3(0, 1, 0);
+        }
+        else
+        {
+            isCrouching_bool = true;
+            anim.SetBool("isCrouching", true);
+            speed = crouchSpeed;
+            col_size.height = 1;
+            col_size.center = new Vector3(0, 0.5f, 0);
+        }
+    }
+
+
+
+}
