0dbceaeb9ec577ae865500a2f12b2a3b2cfcd642
KPennart <kpennart@uncc.edu>
2019-10-19 01:20:49 -0400

    Added Light Flickers

---
+++ Itcs 4231 Game Project/Assets/Scripts/House/LightController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public Light lightSource;
+    private new AudioSource audio;
+
+    private float minIntensity = 0f;
+    private float maxIntensity = 2f;
+    private float lastIntensity = 0;
+
+    public int lightSmoothness = 1;
+
+    private bool flickering = true;
+
+    Queue<float> smoothQueue;
+    
+
+    void Start ()
+    {
+        smoothQueue = new Queue<float>(lightSmoothness);
+
+        lightSource = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            flickering = !flickering;
+        }
+
+        while (smoothQueue.Count >= lightSmoothness && flickering)
+        {
+            lastIntensity -= smoothQueue.Dequeue();
+        }
+
+        if (flickering)
+        {
+            if (Random.Range(0, 10) == 0)
+            {
+                lightSource.intensity = 0;
+            }
+            else
+            {
+                float newVal = Random.Range(minIntensity, maxIntensity);
+                smoothQueue.Enqueue(newVal);
+                lastIntensity += newVal;
+
+                lightSource.intensity = lastIntensity / (float)smoothQueue.Count;
+            }
+        }
+        else
+        {
+            lightSource.intensity = 2f;
+        }
+    }
+}
