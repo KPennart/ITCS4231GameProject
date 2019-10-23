200731ed70095d92da46f685f6d612f62991ae65
KPennart <kpennart@uncc.edu>
2019-10-23 16:18:30 -0400

    Enabled Toggling of Flashlight HUD
    Flashlight HUD now should only display while the flashlight is active.

--- Itcs 4231 Game Project/Assets/Scripts/Player/FlashlightController.cs
+++ Itcs 4231 Game Project/Assets/Scripts/Player/FlashlightController.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FlashlightController : MonoBehaviour
 {
     private Light flashlight;
     private new AudioSource audio;
+    private RawImage flashlightHUD;
 
     [SerializeField] private AudioClip clickOn;
     [SerializeField] private AudioClip clickOff;
@@ -14,6 +16,7 @@ public class FlashlightController : MonoBehaviour
     {
         flashlight = GetComponent<Light>();
         audio = GetComponent<AudioSource>();
+        flashlightHUD = GameObject.Find("Flashlight HUD").GetComponent<RawImage>();
     }
     // Update is called once per frame
     void Update()
@@ -21,6 +24,8 @@ public class FlashlightController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F))
         {
             flashlight.enabled = !flashlight.enabled;
+            flashlightHUD.enabled = !flashlightHUD.enabled;
+
             audio.clip = audio.clip == clickOn ? clickOff : clickOn;
             audio.Play();
         }
