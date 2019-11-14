a63d06fb20e5f65da1dc862091994cba2f63b52f
gldamien <damien.gibbs1996@gmail.com>
2019-11-14 11:27:10 -0500

    Main Menu Added
    Simple Main Menu Added, will update the image when I add more trees, bushes, etc in the exterior scene.

---
+++ Itcs 4231 Game Project/Assets/Scripts/MainMenu/MouseHover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    //called when the mouse enters a collider
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    //called when the mouse exits a collider
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+}
