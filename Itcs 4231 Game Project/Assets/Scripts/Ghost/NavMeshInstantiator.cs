70a8881007e761fff73351ebdbdaae17d798ccf2
KPennart <kpennart@uncc.edu>
2019-11-22 17:20:10 -0500

    Added Proper Navmesh and Navmesh Agent & Minor Decor

---
+++ Itcs 4231 Game Project/Assets/Scripts/Ghost/NavMeshInstantiator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Used to make sure the GO is on the NavMesh.
+/// </summary>
+public class NavMeshInstantiator : MonoBehaviour
+{
+    void Start()
+    {
+        UnityEngine.AI.NavMeshHit closestHit;
+
+        if (UnityEngine.AI.NavMesh.SamplePosition(gameObject.transform.position, out closestHit, 500f, UnityEngine.AI.NavMesh.AllAreas))
+            gameObject.transform.position = closestHit.position;
+        else
+            Debug.LogError("Could not find position on NavMesh!");
+    }
+}
---
+++ Itcs 4231 Game Project/Assets/Scripts/Ghost/PursuePlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PursuePlayer : MonoBehaviour
+{
+    public Transform target;
+    NavMeshAgent agent;
+    public Animator anim;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        agent.SetDestination(target.position);
+        
+        if (agent.remainingDistance > agent.stoppingDistance && !anim.GetBool("isWalking"))
+        {
+            //Debug.Log("true");
+            anim.SetBool("isWalking", true);
+        }
+        else if (agent.remainingDistance <= agent.stoppingDistance && anim.GetBool("isWalking"))
+        {
+            //agent.isStopped = true;
+            //Debug.Log("false");
+            anim.SetBool("isWalking", false);
+        }
+        
+    }
+}
